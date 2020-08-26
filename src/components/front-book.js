4545d121d9e38b5552052b2ab65e8ad37ef4fab0
inksharedsolutions <webdev.inkshared@gmail.com>
2020-08-27 01:28:00 +0800

    changes-8-27-2020

--- src/components/front-book.js
+++ src/components/front-book.js
@@ -35,7 +35,7 @@ const MidBook = (props)=>{
                             </p>
 
                             <button className="btn-book-featured">
-                                <Link to="">
+                                <Link to="/about-the-book">
                                     Read More
                                 </Link>
                             </button>
